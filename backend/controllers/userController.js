1e07c171b97311d619af21c28ccea4a12a36c069
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-03-29 17:46:54 +0530

    Authentication and token generation

---
+++ backend/controllers/userController.js
@@ -0,0 +1,24 @@
+const ErrorHandler = require("../Utils/ErrorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors")
+const User = require("../models/userModel")
+
+//register a user
+exports.registerUser =  catchAsyncErrors(async(req,res,next)=>{
+    const {name,email,password}= req.body;
+    
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar:{
+            public_id:"this is a sample id",
+            url:"profilePicUrl"
+        }
+    });
+    const token = user.getJWTToken();
+
+    res.status(201).json({
+        success :true,
+        token,
+    })
+}) 
