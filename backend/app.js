5110c931136b10c576bbb6d7bb68f28bddfa4ca2
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-03-30 08:51:26 +0530

    token  generation

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app=express();
 const errorMiddleware = require("./middleware/error")
 
 app.use(express.json());
+app.use(cookieParser());
 //route import
  const product = require("./routes/productRoutes")
  const user = require("./routes/userRoute");
